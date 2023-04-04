e7a223b78daf7f1f51f273f7474e5c4dad96a78a
sauliyo15 <gc.saul.7@gmail.com>
2023-04-04 13:46:25 +0200

    Step 1.1.3 Código inicial comentado

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+//Importacion de modulos standard que se han instalado (npm install) procedentes del package.json
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+//Importacion de modulos con los ruters (atencion de rutas)
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+//Creacion de la aplicacion express
+
 var app = express();
 
-// view engine setup
+
+// view engine setup -- define views como directorio que contiene las vistas
 app.set('views', path.join(__dirname, 'views'));
+
+//Instalacion de un renderizador de vistas
 app.set('view engine', 'ejs');
+
+
+//Instalacion de MWs genericos
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,10 +33,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//Instalacion de MWs de rutas
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+
+// catch 404 and forward to error handler - Instalacion de MWs de Error
 app.use(function(req, res, next) {
   next(createError(404));
 });
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //res.locals.title = "Expressito"; //Otra forma de mandar el parametro
   res.render('index', { title: 'Express' });
 });
 
