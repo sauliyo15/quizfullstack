b87fd6b61534f95576d25f1954d5d149ea1d33b3
sauliyo15 <gc.saul.7@gmail.com>
2023-04-10 13:25:48 +0200

    Step 1.2.4 Añadir página de autor e imágenes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.locals.title = "Expressito"; //Otra forma de mandar el parametro
   res.render('index', { title: 'Welcome to Quiz', message: 'The Web site where you can create you own games!' });
   
+  //res.locals.title = "Expressito"; //Otra forma de mandar el parametro
   //res.render('index'); //Se pueden eliminar los parametros e insertar directamente los mensajes en el codigo html de la vista index.
+});
+
+
+//Author page
+router.get('/author', (req, res, next) => {
+  res.render('author');
 });
 
 module.exports = router;
