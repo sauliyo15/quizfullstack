0d1e48fd7596dcd5783ac7e48ea894a3ec62c21c
sauliyo15 <gc.saul.7@gmail.com>
2023-04-10 11:26:45 +0200

    Step 1.2.1 Modificar Home Page (mensajes bienvenida)

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.locals.title = "Expressito"; //Otra forma de mandar el parametro
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to Quiz', message: 'The Web site where you can create you own games!' });
+  
+  //res.render('index'); //Se pueden eliminar los parametros e insertar directamente los mensajes en el codigo html de la vista index.
 });
 
 module.exports = router;
