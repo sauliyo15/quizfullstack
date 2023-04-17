c850c0e56908f0c85b9812178aac170436e6071f
sauliyo15 <gc.saul.7@gmail.com>
2023-04-17 18:13:51 +0200

    Step 2.1c.1.1 Modificando version sequelize/sequelize-cli version<6 - Funcionamiento autoload OK

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,17 +1,17 @@
+const { models } = require("../models");
+
 //Autoload el quiz asociado a :quizId
 exports.load = async (req, res, next, quizId) => {
-    try {
-      const quiz = await models.Quiz.findByPk(quizId);
-      if (quiz) {
-        req.load = {...req.load, quiz}; //... spread
-        next();
-      }
-      else {
-        throw new Error('There is no quiz with id= ' + quizId);
-      }
+  try {
+    const quiz = await models.Quiz.findByPk(quizId);
+    if (quiz) {
+      req.load = {...req.load, quiz};
+      next();
     }
-    catch (error) {
-      next(error);
+    else {
+      throw new Error('There is no quiez with id=' + quizId);
     }
-  };
-  
+  } catch (error) {
+    next(error);
+  }
+};
--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 //Direccion (o fichero) de la base de datos
-const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
+const url = process.env.DATABASE_URL || "sqlite:db.sqlite";
 
 const sequelize = new Sequelize(url);
 
-//Importar el modelo Quiz
+//Importar el modelo Quiz - OJO! IMPORTANTE este metodo no funciona a partir de la version 6 de sequelize/sequelize-cli
 sequelize.import(path.join(__dirname, 'quiz'));
 
-module.exports = sequelize;
 
+module.exports = sequelize;
